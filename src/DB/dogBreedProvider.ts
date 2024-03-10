02a0c78e0023fc30cde76a08fb913d3e3fe9ecb2
Michael manley <mmanle01@gmail.com>
2024-03-10 09:25:59 -0400

    added default .env.local to make it easier to connect to the database and have default test configurations. Updated DogBreedProvider to have interface for the object returned by the query. Added try/catch around queries.

--- src/DB/dogBreedProvider.ts
+++ src/DB/dogBreedProvider.ts
@@ -1,26 +1,37 @@
 import * as db from './postgres'
 
 interface DogBreedRepository {
-  getDogBreeds(): Promise<any>
-  getDogBreedsByBreedIdLike(idLike: string) : Promise<any>
+  getDogBreeds(): Promise<DogBreed[]>
+  getDogBreedsByBreedIdLike(idLike: string): Promise<DogBreed[]>
+}
+
+interface DogBreed {
+  id: string
+  shortDescription: string
+  longDescription: string
+  imagePath: string
 }
 
 class PostgresDogBreedProvider implements DogBreedRepository {
 
-  async getDogBreeds(): Promise<any> {
-    return JSON.stringify(
-      (
+  async getDogBreeds(): Promise<DogBreed[]> {
+    try {
+      return (
         await db.query("SELECT * FROM dog_breed", [])
       ).rows
-    )
+    } catch (error: any) {
+      throw new Error("Error fetching dog breeds: " + error.message);
+    }
   }
 
-  async getDogBreedsByBreedIdLike(idLike: string): Promise<any> {
-    return JSON.stringify(
-      (
+  async getDogBreedsByBreedIdLike(idLike: string): Promise<DogBreed[]> {
+    try {
+      return (
         await db.query("select * from dog_breed db where db.id like ('%$1%')", [idLike])
       ).rows
-    )
+    } catch (error: any) {
+      throw new Error("Error fetching dog breeds: " + error.message);
+    }
   }
 }
 
--- src/DB/postgres.ts
+++ src/DB/postgres.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: ['.env.local', '.env'] })
 import { Client, Pool } from 'pg'
 
 
