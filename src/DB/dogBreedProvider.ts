f80c50dd8c1e2d85260fb1d954ba1042b2a0cf4b
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-03-13 18:36:07 +0100

    Adding post request route

--- src/DB/dogBreedProvider.ts
+++ src/DB/dogBreedProvider.ts
@@ -1,38 +1,58 @@
-import * as db from './postgres'
+import * as db from './postgres';
 
 interface DogBreedRepository {
-  getDogBreeds(): Promise<DogBreed[]>
-  getDogBreedsByBreedIdLike(idLike: string): Promise<DogBreed[]>
+  getDogBreeds(): Promise<DogBreed[]>;
+  getDogBreedsByBreedIdLike(idLike: string): Promise<DogBreed[]>;
 }
 
 interface DogBreed {
-  id: string
-  shortDescription: string
-  longDescription: string
-  imagePath: string
+  id: string;
+  shortDescription: string;
+  longDescription: string;
+  imagePath: string;
+}
+
+export interface NewDogBreed {
+  id: string;
+  shortDescription: string;
+  longDescription: string;
+  imagePath: string;
 }
 
 class PostgresDogBreedProvider implements DogBreedRepository {
-
   async getDogBreeds(): Promise<DogBreed[]> {
     try {
-      return (
-        await db.query("SELECT * FROM dog_breed", [])
-      ).rows
+      return (await db.query('SELECT * FROM dog_breed', [])).rows;
     } catch (error: any) {
-      throw new Error("Error fetching dog breeds: " + error.message);
+      throw new Error('Error fetching dog breeds: ' + error.message);
     }
   }
 
   async getDogBreedsByBreedIdLike(idLike: string): Promise<DogBreed[]> {
     try {
       return (
-        await db.query("select * from dog_breed db where db.id like ('%$1%')", [idLike])
-      ).rows
+        await db.query("select * from dog_breed db where db.id like ('%$1%')", [
+          idLike,
+        ])
+      ).rows;
     } catch (error: any) {
-      throw new Error("Error fetching dog breeds: " + error.message);
+      throw new Error('Error fetching dog breeds: ' + error.message);
+    }
+  }
+
+  async createDogBreed(newDogBreed: NewDogBreed): Promise<void> {
+    try {
+      const { id, shortDescription, longDescription, imagePath } = newDogBreed;
+      return (
+        await db.query(
+          'INSERT INTO dog_breed (id, short_description, long_description, image_path) VALUES ($1, $2, $3, $4) RETURNING *',
+          [id, shortDescription, longDescription, imagePath]
+        )
+      ).rows[0];
+    } catch (error: any) {
+      throw new Error('Error creating dog breed: ' + error.message);
     }
   }
 }
 
-export default new PostgresDogBreedProvider()
+export default new PostgresDogBreedProvider();
