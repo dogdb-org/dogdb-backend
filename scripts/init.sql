c6bf1edea94d418d9e79bd1b2600e7de2ca93b12
Michael manley <mmanle01@gmail.com>
2024-03-17 12:08:59 -0400

    Fix init.sql file schema issues, and setup postgres db service in docker to automatically load the init.sql when creating/starting the image.

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,12 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS dogpedia;
+SET search_path TO dogpedia,public;
 
-CREATE TABLE dogpedia.dog_breed (
+CREATE TABLE dog_breed (
     id varchar(150) PRIMARY KEY,
     short_description varchar(150),
     long_description varchar(1024),
     image_path varchar(255)
 );
-ALTER DATABASE postgres SET search_path TO dogpedia;
+
 
 INSERT INTO dog_breed (id, short_description, long_description, image_path)
 values ('German Shephard', 'This is a smart puppy.', 'This is a really, really smart puppy.', '/images/german-shephard.jpg');
