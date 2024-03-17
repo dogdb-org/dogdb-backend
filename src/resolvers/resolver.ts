c497bd9abb93dcf8ad9ccfa17abd8dbfd24f4844
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-03-17 10:34:41 +0100

    Adding createDogBreed and getDogBreed features

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -4,15 +4,52 @@ import * as db from '../DB/postgres';
 
 // import jwt from 'jsonwebtoken';
 // import bcrypt from 'bcrypt';
+interface DogBreed {
+  id: string;
+  short_description: string;
+  long_description: string;
+  image_path: string;
+}
+interface NewDogBreed {
+  id: string;
+  short_description: string;
+  long_description: string;
+  image_path: string;
+}
 
 const resolvers = {
   Query: {
     hello: () => {
       return 'Hello, world!';
     },
-    getDogBreeds: async () => {
+    getDogBreeds: async (): Promise<DogBreed[]> => {
       const { rows } = await db.query('SELECT * FROM dog_breed', []);
       return rows;
+    },
+    getDogBreed: async (_: any, args: { id: string }): Promise<DogBreed[]> => {
+      const { id } = args;
+      const { rows } = await db.query('SELECT * FROM dog_breed WHERE id = $1', [
+        id,
+      ]);
+      return rows[0];
+    },
+  },
+  Mutation: {
+    createDogBreed: async (
+      _: any,
+      args: {
+        id: string;
+        short_description: string;
+        long_description: string;
+        image_path: string;
+      }
+    ): Promise<NewDogBreed> => {
+      const { id, short_description, long_description, image_path } = args;
+      const { rows } = await db.query(
+        'INSERT INTO dog_breed (id, short_description, long_description, image_path) VALUES ($1, $2, $3, $4) RETURNING *',
+        [id, short_description, long_description, image_path]
+      );
+      return rows[0];
     },
   },
 };
--- src/schemas/schema.ts
+++ src/schemas/schema.ts
@@ -9,6 +9,16 @@ const typeDefs = `
   type Query {
     hello: String
     getDogBreeds: [DogBreed]
+    getDogBreed(id: String!): DogBreed
+  }
+
+  type Mutation {
+    createDogBreed(
+      id: String!
+      short_description: String
+      long_description: String
+      image_path: String
+    ): DogBreed
   }
 `;
 
